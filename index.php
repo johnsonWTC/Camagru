559577238b96451dcebcc4ab93ff3f40c25c6cd5
Mcebo XABA <mxaba@e4r3p2.wethinkcode.co.za>
2018-10-15 16:20:43 +0200

    Nothing is there

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    // require_once('header.php');
+    // $rootDir = '.';
+    // if (goHeader('Camagru', "LOG", $rootDir) == FALSE)
+    //     return ;
+?>
+hi
+    <div class = "loading" sysle = "opacity = 0">
+    <div class = "bar0"></div>
+    <div class = "bar1"></div>
+    <div class = "bar2"></div>
+    <div class = "bar3"></div>
+    <div class = "bar4"></div>
+    </div>
+<!-- <script defer src = "tools.js" charset = "utf-8"></script>
+<script defer src = "all_images.js" charset = "utf-8"></script> -->
+<?php
+     require_once($rootDir . '/footer.html')
+?>
